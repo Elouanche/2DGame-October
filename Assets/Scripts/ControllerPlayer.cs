9722fdf9ad431c6bc1d55f7eb36c17295cfc0508
Elouan CHE <elouan.chedalleux@ynov.com>
2024-10-18 10:52:09 +0200

    Add jump

--- Assets/Scripts/ControllerPlayer.cs
+++ Assets/Scripts/ControllerPlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ControllerPlayer : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+
+    public bool isJumping = false;
 
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
@@ -20,6 +23,11 @@ public class ControllerPlayer : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
         MovePlayer(horizontalMovement);
     }
 
@@ -27,5 +35,11 @@ public class ControllerPlayer : MonoBehaviour
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping=false;
+        }
     }
 }
